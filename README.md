cd771b50e2cd471fb5f880b938c2d483a3cac58d
chipdelmal <chipdelmal@gmail.com>
2022-03-29 13:27:58 -0700

    Setting up files to test or-tools tests

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 # Sources
 
 * https://www.educative.io/m/find-all-sum-combinations
+* https://stackoverflow.com/questions/42422921/multiple-subset-sum-calculation
+* https://developers.google.com/optimization/bin/multiple_knapsack
--- functions.py
+++ functions.py
@@ -9,3 +9,35 @@ def runLength(s_list):
 def flatten(t):
     # https://stackoverflow.com/questions/952914/how-to-make-a-flat-list-out-of-a-list-of-lists
     return [item for sublist in t for item in sublist]
+
+
+class NotFound(BaseException):
+    pass
+
+from collections import defaultdict
+def subsetsum(A,N):
+    res=[[0]]+[[] for i in range(N)]
+    for i,a in enumerate(A):
+        k=1<<i        
+        stop=[len(l) for l in res] 
+        for shift,l in enumerate(res[:N+1-a]):
+            n=a+shift   
+            ln=res[n]
+            for s in l[:stop[shift]]: ln.append(s+k)
+    return res
+
+res = subsetsum(A,max(B))
+solB = [res[b] for b in B]
+exactsol = ~-(1<<len(A))
+
+def decode(answer):
+    return [[A[i] for i,b in enumerate(bin(sol)[::-1]) if b=='1'] for sol in answer] 
+
+def solve(i,currentsol,answer):
+        if currentsol==exactsol : print(decode(answer))
+        if i==len(B): return
+        for sol in solB[i]:
+                if not currentsol&sol:
+                    answer.append(sol)
+                    solve(i+1,currentsol+sol,answer)
+                    answer.pop()
