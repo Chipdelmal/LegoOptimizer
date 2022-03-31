8317c56970dee4f1ed95bb2b13fbb36d0686fe9b
chipdelmal <chipdelmal@gmail.com>
2022-03-30 17:01:00 -0700

    Wrapping up optimizer functions

--- functions.py
+++ functions.py
@@ -1,6 +1,11 @@
+
+import constants as cst
 from itertools import groupby
 
 
+###############################################################################
+# Auxiliary
+###############################################################################
 def runLength(s_list):
     # https://www.w3resource.com/python-exercises/list/python-data-type-list-exercise-75.php
     grp = groupby(s_list)
@@ -10,3 +15,46 @@ def runLength(s_list):
 def flatten(t):
     # https://stackoverflow.com/questions/952914/how-to-make-a-flat-list-out-of-a-list-of-lists
     return [item for sublist in t for item in sublist]
+
+###############################################################################
+# Optimization
+###############################################################################
+def genSolverData(gaps, blocks, values=cst.NULL_BLOCK_VALUES):
+    data = {}
+    data['weights'] = blocks
+    data['values'] = [values[i] for i in blocks]
+    assert len(data['weights']) == len(data['values'])
+    data['num_items'] = len(data['weights'])
+    data['all_items'] = range(data['num_items'])
+    data['bin_capacities'] = gaps
+    data['num_bins'] = len(data['bin_capacities'])
+    data['all_bins'] = range(data['num_bins'])
+    return data
+
+def genSolverXVector(data, solver):
+    # x[i, b] = 1 if item i is packed in bin b
+    x = {}
+    for i in data['all_items']:
+        for b in data['all_bins']:
+            x[i, b] = solver.BoolVar(f'x_{i}_{b}')
+    return (x, solver)
+
+def setSolverConstraints(data, x, solver):
+    # Each item is assigned to at most one bin.
+    for i in data['all_items']:
+        solver.Add(sum(x[i, b] for b in data['all_bins']) <= 1)
+    # The amount packed in each bin cannot exceed its capacity.
+    for b in data['all_bins']:
+        solver.Add(
+            sum(x[i, b] * data['weights'][i]
+                for i in data['all_items']) <= data['bin_capacities'][b])
+    return (x, solver)
+
+
+def setSolverObjective(data, x, solver):
+    objective = solver.Objective()
+    for i in data['all_items']:
+        for b in data['all_bins']:
+            objective.SetCoefficient(x[i, b], data['values'][i])
+    objective.SetMaximization()
+    return (x, solver, objective)
--- optimizer.py
+++ optimizer.py
@@ -1,53 +1,27 @@
 
 import constants as cst
 from ortools.linear_solver import pywraplp
+import functions as fun
 
 ###############################################################################
-# Problem input
+# Problem Input
 ###############################################################################
-gaps = [4, 2, 6, 1]
-blocks = [1, 1, 1, 1, 2, 2, 2, 3, 3, 3]
-values = [cst.NULL_BLOCK_VALUES[i] for i in blocks]
+gaps = [4, 2, 6, 2]
+blocks = [1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3]
+values = cst.NULL_BLOCK_VALUES
+data = fun.genSolverData(gaps, blocks, values=values)
 ###############################################################################
 # Problem Setup
 ###############################################################################
-data = {}
-data['weights'] = blocks
-data['values'] = values
-assert len(data['weights']) == len(data['values'])
-data['num_items'] = len(data['weights'])
-data['all_items'] = range(data['num_items'])
-
-data['bin_capacities'] = gaps
-data['num_bins'] = len(data['bin_capacities'])
-data['all_bins'] = range(data['num_bins'])
-# Create the mip solver with the SCIP backend.
 solver = pywraplp.Solver.CreateSolver('SCIP')
-# Variables.
-# x[i, b] = 1 if item i is packed in bin b.
-x = {}
-for i in data['all_items']:
-    for b in data['all_bins']:
-        x[i, b] = solver.BoolVar(f'x_{i}_{b}')
-# Constraints.
-# Each item is assigned to at most one bin.
-for i in data['all_items']:
-    solver.Add(sum(x[i, b] for b in data['all_bins']) <= 1)
-# The amount packed in each bin cannot exceed its capacity.
-for b in data['all_bins']:
-    solver.Add(
-        sum(x[i, b] * data['weights'][i]
-            for i in data['all_items']) <= data['bin_capacities'][b])
-# Objective: Maximize total value of packed items.
-objective = solver.Objective()
-for i in data['all_items']:
-    for b in data['all_bins']:
-        objective.SetCoefficient(x[i, b], data['values'][i])
-objective.SetMaximization()
-
+(x, solver) = fun.genSolverXVector(data, solver)
+(x, solver) = fun.setSolverConstraints(data, x, solver)
+(x, solver, objective) = fun.setSolverObjective(data, x, solver)
+###############################################################################
+# Objective
+###############################################################################
 
 status = solver.Solve()
-
 if status == pywraplp.Solver.OPTIMAL:
     print(f'Total packed value: {objective.Value()}')
     total_weight = 0
