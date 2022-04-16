d2c5769daac1077c2d5e20983aa0df5995008619
chipdelmal <chipdelmal@gmail.com>
2022-04-16 15:36:01 -0700

    Minor sync change

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ import constants as cst
 # USER-SELECTIONS
 ############################################################################### 
 USER_SEL = {
-    'size': [int(i*1) for i in (28, 36)],
+    'size': [int(i*1) for i in (50, 50)],
     'palette':  12, #cst.LEGO_LIMITED, # 6,
     'blocks': cst.LIMITLESS_BLOCKS_SUPPLY,
     'priority': cst.LARGE_FIRST_BLOCK_VALUES,
